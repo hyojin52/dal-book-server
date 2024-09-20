89a27feabd946df390a7e45ba894490a5dab57e8
hyojin <dongurami.kim@gmail.com>
2024-09-20 15:38:43 +0900

    feat: response 타입 정의

---
+++ src/main/java/org/project/dalantbook/controller/response/Response.java
@@ -0,0 +1,37 @@
+package org.project.dalantbook.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static Response<Void> error(String errorCode) {
+        return new Response<>(errorCode, null);
+    }
+
+    public static Response<Void> success() {
+        return new Response<>("SUCCESS", null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", result);
+    }
+
+    public String toStream() {
+        if(result == null) {
+            return "{ \n" +
+                    "  \"resultCode\":" + "\"" + resultCode + "\", \n" +
+                    "  \"result\":" + null +
+                    "\n }";
+        }
+
+        return "{ \n" +
+                "  \"resultCode\":" + "\"" + resultCode + "\", \n" +
+                "  \"result\":" + "\"" + result + "\"" +
+                "\n }";
+    }
+}
