0d41153bb3a7acf8e6b18a56345038c6d615d4e1
hyojin <dongurami.kim@gmail.com>
2024-09-20 16:03:58 +0900

    feat: health controller api 응답 형식 수정

--- src/main/java/org/project/dalantbook/controller/HealthController.java
+++ src/main/java/org/project/dalantbook/controller/HealthController.java
@@ -1,5 +1,6 @@
 package org.project.dalantbook.controller;
 
+import org.project.dalantbook.controller.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping
-    public String health() {
-        return "OK";
+    public Response<String> health() {
+        return Response.success("OK");
     }
 
 }
