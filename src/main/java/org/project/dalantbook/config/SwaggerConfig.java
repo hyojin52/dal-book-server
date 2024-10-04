e6a124077dd7e3683750d1941d3715cd43a914b9
hyojin <dongurami.kim@gmail.com>
2024-10-04 15:57:56 +0900

    feat: swagger authentication header 추가

--- src/main/java/org/project/dalantbook/config/SwaggerConfig.java
+++ src/main/java/org/project/dalantbook/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package org.project.dalantbook.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -23,5 +27,16 @@ public class SwaggerConfig {
                 .group("DalBook API v1")  // 그룹 이름을 설정한다.
                 .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
